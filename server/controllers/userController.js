5aeabe5715bfe6eab060c249372bdf7395c16d70
Chaitanya Kelkar <chaitanyaakelkar57@gmail.com>
2025-06-17 20:56:06 +0530

    added more routes models controllers in backend

---
+++ server/controllers/userController.js
@@ -0,0 +1,63 @@
+import { clerkClient } from "@clerk/express"
+import Booking from "../models/Booking.js"
+import Movie from "../models/Movie.js"
+
+
+//api controller function to get user bookings
+export const getUserBookings = async (req, res) => {
+    try {
+        const user = req.auth().userId
+
+        const bookings = await Booking.find({user}).populate({
+            path: 'show',
+            populate: {path: 'movie'}
+        }).sort({createdAt: -1})
+
+        res.json({success: false, bookings})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
+}
+
+//api controller function to update favourite movie in clerk user metadata
+export const updateFavorite = async (req, res) => {
+    try {
+        const {movieId} = req.body
+        const userId = req.auth().userId
+
+        const user = await clerkClient.users.getUser(userId)
+
+        if(!user.privateMetadata.favorites){
+            user.privateMetadata.favorites = []
+        }
+
+        if(!user.privateMetadata.favorites.includes(movieId)){
+            user.privateMetadata.favorites.push(movieId)
+        } else {
+            user.privateMetadata.favorites = user.privateMetadata.favorites.filter(id => id !== movieId)
+        }
+
+        await clerkClient.users.updateUserMetadata(userId, {privateMetadata: user.privateMetadata})
+
+        res.json({success: true, message: "Favorites movie updated successfully!"})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
+}
+
+export const getFavourites = async (req, res) => {
+    try {
+        const user = await clerkClient.users.getUser(req.auth().userId)
+        const favorites = user.privateMetadata.favorites
+
+        //get movies from database
+        const movies = await Movie.find(_id: {$in: favorites})
+
+        res.json({success: true, movies})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
+}
