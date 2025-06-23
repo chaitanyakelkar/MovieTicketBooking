070fa2ddaa8cab5bd951c1e354e603dd1cbba065
Chaitanya Kelkar <chaitanyaakelkar57@gmail.com>
2025-06-23 12:59:52 +0530

    displaying timer with loading animation

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,22 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 const Loading = () => {
 
   const {nextUrl} = useParams()
   const navigate = useNavigate()
+  const [time, setTime] = useState()
 
   useEffect(() => {
     if (nextUrl){
-      setTimeout(()=>{
-        navigate('/' + nextUrl)
-      }, 8000)
+      setTime(8)
+      const intervalId = setInterval(() => {
+        setTime(prev => {
+          if (prev === 1){
+            clearInterval(intervalId)
+            setTimeout(() => navigate("/" + nextUrl), 0)
+          }
+          return prev - 1})
+      }, 1000)
+
+      return () =>{
+        clearInterval(intervalId)
+      }
     }
-  })
+  }, [nextUrl, navigate])
 
   return (
-    <div className='flex justify-center items-center h-[80vh]'>
+    <div className='flex justify-center items-center h-[80vh] flex-col'>
         <div className='animate-spin rounded-full h-14 w-14 border-2 border-t-primary'></div>
+        {nextUrl && <div className='mt-2'>Wait for <span>{time}</span> seconds...</div>}
+        {!nextUrl && <div className='mt-2'>Page is Loading...</div>}
     </div>
   )
 }
